38beb44ee522913cdb81b052b2c9983fb8a8fafb
mjkrause <michael.j.krause@gmail.com>
2018-03-09 13:31:07 -0800

    corrections in Makefile and subst_config_vars.py

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 SHELL := /bin/bash
 deploy:
-	@echo "installing dependencies\n"
+	@echo "installing dependencies"
 	pip install -r requirements.txt
 	source .env
-	@echo "configuring AWS with your credentials\n"
+	@echo "configuring AWS with your credentials"
 	pip install awscli --upgrade
 	aws configure
 	chalice new-project dss-blau
@@ -12,12 +12,12 @@ deploy:
 	@echo "update lambda configuration parameters"
 	python subst_config_vars.py
 	cp -r chalicelib dss-blau/chalicelib
-	@echo "deploying lambda on AWS\n"
+	@echo "deploying lambda on AWS"
 	cd dss-blau && \
 	chalice deploy --no-autogen-policy
 
 deploy-local:
-	@echo "deploying AWS local\n"
+	@echo "deploying AWS local"
 	cd dss-blau && \
 	chalice local
 
--- subst_config_vars.py
+++ subst_config_vars.py
@@ -10,21 +10,22 @@ def subst_config_vars():
     to .chalice directory."""
 
     fname = 'dss-blau/.chalice/config.json'
+    with open(fname, 'r') as fp:
+        config = json.load(fp)
     # Get user-specific values.
-    iam_role_arn = os.environ["IAM_ROLE_ARN"]
+    config['stages']['dev']['manage_iam_role'] = False
+    config['stages']['dev']['iam_role_arn'] = os.environ["IAM_ROLE_ARN"]
     es_endpoint = os.environ["ES_ENDPOINT"]
     blue_box_endpoint = os.environ["BLUE_BOX_ENDPOINT"]
     es_index = os.environ["ES_INDEX"]
     indexer_name = os.environ["INDEXER_NAME"]
     home = os.environ["HOME"]
     # Create dict from those.
-    config_vars = [iam_role_arn, es_endpoint, blue_box_endpoint,
-                   es_index, indexer_name, home]
-    config_keys = ['IAM_ROLE_ARN', 'ES_ENDPOINT', 'BLUE_BOX_ENDPOINT',
+    config_keys = ['ES_ENDPOINT', 'BLUE_BOX_ENDPOINT',
                    'ES_INDEX', 'INDEXER_NAME', 'HOME']
+    config_vars = [es_endpoint, blue_box_endpoint,
+                   es_index, indexer_name, home]
     config_params = dict(zip(config_keys, config_vars))
-    with open(fname, 'r') as fp:
-        config = json.load(fp)
     config['stages']['dev'].update(config_params)
     with open(fname, 'w') as fp:
         json.dump(config, fp)
