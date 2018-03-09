55bb2437aedd285853c2b64049aca2b0c34ede01
mjkrause <michael.j.krause@gmail.com>
2018-03-09 09:30:49 -0800

    added Python code to append / substitute Lambda parameters, changed Makefile accordingly

--- Makefile
+++ Makefile
@@ -4,11 +4,15 @@ deploy:
 	pip install -r requirements.txt
 	source .env
 	@echo "configuring AWS with your credentials\n"
-	pip install awscli --upgrade && \
-	aws configure && \
-	chalice new-project dss-blau && \
+	pip install awscli --upgrade
+	aws configure
+	chalice new-project dss-blau
 	cp app.py dss-blau/app.py
+	cp requirements.txt dss-blau/requirements.txt
+	@echo "update lambda configuration parameters"
+	python subst_config_vars.py
 	cp -r chalicelib dss-blau/chalicelib
+	cp -r ../dss-blau_copy_2018-03-02/.chalice dss-blau
 	@echo "deploying lambda on AWS\n"
 	cd dss-blau && \
 	chalice deploy --no-autogen-policy
---
+++ subst_config_vars.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import json
+
+
+def subst_config_vars():
+    """Obtain user values for AWS from environment variables. Then
+    sustitute the value by updating config.json and write back
+    to .chalice directory."""
+
+    fname = 'dss-blau/.chalice/config.json'
+    # Get user-specific values.
+    iam_role_arn = os.environ["IAM_ROLE_ARN"]
+    es_endpoint = os.environ["ES_ENDPOINT"]
+    blue_box_endpoint = os.environ["BLUE_BOX_ENDPOINT"]
+    es_index = os.environ["ES_INDEX"]
+    indexer_name = os.environ["INDEXER_NAME"]
+    home = os.environ["HOME"]
+    # Create dict from those.
+    config_vars = [iam_role_arn, es_endpoint, blue_box_endpoint,
+                   es_index, indexer_name, home]
+    config_keys = ['IAM_ROLE_ARN', 'ES_ENDPOINT', 'BLUE_BOX_ENDPOINT',
+                   'ES_INDEX', 'INDEXER_NAME', 'HOME']
+    config_params = dict(zip(config_keys, config_vars))
+    with open(fname, 'r') as fp:
+        config = json.load(fp)
+    config.update(config_params)
+    with open(fname, 'w') as fp:
+        json.dump(config, fp)
+
+
+def main():
+    subst_config_vars()
+
+
+if __name__ == '__main__':
+    main()
