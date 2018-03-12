08aad29a856aa440e089b44ad1d8313b5239f9c1
mjkrause <michael.j.krause@gmail.com>
2018-03-12 11:26:54 -0700

    using boto to get AWS acc num

--- Makefile
+++ Makefile
@@ -3,17 +3,16 @@ SHELL := /bin/bash
 deploy:
 	@echo "installing dependencies"
 	pip install -r requirements.txt
-	source .env
 	@echo "configuring AWS with your credentials"
 	pip install awscli --upgrade
-	aws configure
-	export ACC_NUM="$(aws sts get-caller-identity --query 'Account')"
+	aws configure 
 	@read -p "Enter AWS-lambda name: " lambda_name; \
 	chalice new-project $$lambda_name; \
 	cp app.py $$lambda_name/app.py; \
 	cp requirements.txt $$lambda_name/requirements.txt; \
-	python subst_config_vars.py $$lambda_name; \
-	cp -r chalicelib $$lambda_name/chalicelib; \
+	source .env \
+	python subst_config_vars.py $$lambda_name;
+	cp -r chalicelib $$lambda_name/chalicelib;
 	cd $$lambda_name && \
 	chalice deploy --no-autogen-policy
 
--- subst_config_vars.py
+++ subst_config_vars.py
@@ -3,6 +3,7 @@
 import os
 import json
 import click
+import boto3
 
 
 @click.command()
@@ -12,14 +13,15 @@ def subst_config_vars(lambda_name):
     sustitute the value by updating config.json and write back
     to .chalice directory."""
     # Get account number.
-    acc_num = os.environ["ACC_NUM"].strip('""')
+    iam = boto3.resource('iam')
+    account_id = iam.CurrentUser().arn.split(":")[4]
     # aws sts get-caller-identity --query 'Account'
     fname = lambda_name + '/.chalice/config.json'
     with open(fname, 'r') as fp:
         config = json.load(fp)
     # Get user-specific values.
     config['stages']['dev']['manage_iam_role'] = False  # encoded by json pack.
-    config['stages']['dev']['iam_role_arn'] = ('arn:aws:iam::' + acc_num +
+    config['stages']['dev']['iam_role_arn'] = ('arn:aws:iam::' + account_id +
                                                ':role/' + lambda_name + '-dev')
     es_endpoint = os.environ["ES_ENDPOINT"]
     blue_box_endpoint = os.environ["BLUE_BOX_ENDPOINT"]
